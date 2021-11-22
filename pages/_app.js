485bd109c2fbb25bc595a9e7f7c3045abe62c700
Neil Sabde <neilsabde@gmail.com>
2021-11-22 08:41:27 +0530

    Remove code highlight

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,11 @@
-import { useEffect } from "react";
-import { MDXProvider } from "@mdx-js/react";
-import {
-  h1,
-  p,
-  a,
-  blockQuote,
-  code,
-  inlineCode,
-  image,
-  link,
-} from "../components/tags";
-
+import {useEffect} from "react";
+import {MDXProvider} from "@mdx-js/react";
 import "../styles/globals.css";
 
-import hljs from "highlight.js";
-import "highlight.js/styles/default.css";
-
-import lua from "highlight.js/lib/languages/lua";
-import bash from "highlight.js/lib/languages/bash";
-
-hljs.registerLanguage("lua", lua);
-hljs.registerLanguage("bash", bash);
-
-const mdComponents = {
-  h1: h1,
-  p: p,
-  a: a,
-  blockquote: blockQuote,
-  code: code,
-  inlineCode: inlineCode,
-  img: image,
-  link: link,
-};
-
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    hljs.initHighlighting();
-  }, []);
+function MyApp({Component, pageProps}) {
   return (
     <>
-      <MDXProvider components={mdComponents}>
+      <MDXProvider>
         <Component {...pageProps} />
       </MDXProvider>
     </>
